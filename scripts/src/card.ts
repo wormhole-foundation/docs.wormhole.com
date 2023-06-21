a09150dcb103208c544d60f11c87d211a092e9f2
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-06-21 14:11:26 -0400

    Terminology: Replace xDapp with Cross Chain Application

--- scripts/src/card.ts
+++ scripts/src/card.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { DocChain } from "./config"
 
 
-const ICONS_PATH = "../.gitbook/assets/chain-icons"
+const ICONS_PATH = "/.gitbook/assets/chain-icons"
 
 export function chainCard(dc: DocChain): string {
     const {name} = dc
@@ -14,16 +14,16 @@ export function chainCard(dc: DocChain): string {
     if(title === undefined) title = name
 
 
-    let icon = `${ICONS_PATH}/${name}.svg`
-    if(!fs.existsSync("../docs/overview/" + icon)){
-        icon = `${ICONS_PATH}/${name}.png`
-    }
+    const icon = `${ICONS_PATH}/${name}.svg`
+    //if(!fs.existsSync("../docs/" + icon)){
+    //    icon = `${ICONS_PATH}/${name}.png`
+    //}
 
 
 
 return `<tr>
     <td><strong>${title}</strong></td>
-    <td><a href="../reference/environments/${environment}.md#${name}">${environment}.md</a></td>
+    <td><a href="/reference/environments/${environment}.md#${name}">${environment}.md</a></td>
     <td><a href="${icon}">${name}</a></td>
 </tr>`
 }
