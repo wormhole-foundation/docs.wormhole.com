289a1edbc3a91207129d6d92aab61eb476ef416c
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-06-28 14:52:05 -0400

    link experiment

--- scripts/scripts/extract_hello_world.sh
+++ scripts/scripts/extract_hello_world.sh
@@ -6,7 +6,7 @@ echo "Grab hello-world tutorial content"
 
 git clone git@github.com:wormhole-foundation/hello-wormhole.git
 
-cp hello-wormhole/README.md ../../docs/tutorials/quick-start/hello-wormhole.md
-cp hello-wormhole/HOW_DOES_IT_WORK.md ../../docs/tutorials/quick-start/hello-wormhole-explainer.md
+cp hello-wormhole/README.md ../../docs/tutorials/quick-start/hello-wormhole/
+cp hello-wormhole/HOW_DOES_IT_WORK.md ../../docs/tutorials/quick-start/hello-wormhole/hello-wormhole-explained.md
 
 rm -rf hello-wormhole 
