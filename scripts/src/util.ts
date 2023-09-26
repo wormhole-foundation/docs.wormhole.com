78d93534fb824c907da186f14376d7c6c749df14
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-09-26 08:30:09 -0400

    adding real tables to try better formatting

--- scripts/src/util.ts
+++ scripts/src/util.ts
@@ -1,9 +1,11 @@
-
 export function fmtNum(n?: number): string {
-    return n === undefined ? " " : n.toString();
+  return n === undefined ? " " : n.toString();
 }
-  
+
 export function fmtStr(s?: string): string {
-    return s === undefined ? "**N/A**" : `\`${s}\``;
-  }
-  
+  return s === undefined ? "**N/A**" : `\`${s}\``;
+}
+
+export function fmtCodeStr(s?: string): string {
+  return s === undefined ? "<code>-</code>" : `<code>${s}</code>`;
+}
