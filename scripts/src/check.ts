f4004a809f50ae1409e6203640495c11a83a02aa
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-11-06 10:20:52 -0500

    fix incorrec tchain ids

--- scripts/src/check.ts
+++ scripts/src/check.ts
@@ -1,4 +1,6 @@
-import { ChainName, Network, chains, CONFIG as newconf, toChainId } from '@wormhole-foundation/connect-sdk'
+import * as cfg from "./config";
+
+import { ChainConfig as SdkChainConfig, Contracts as SdkContracts, ChainName, Network, chains, CONFIG as newconf, toChainId } from '@wormhole-foundation/connect-sdk'
 import { CHAINS as whchains, relayer, CONTRACTS as whcontracts } from '@certusone/wormhole-sdk'
 
 
@@ -15,12 +17,12 @@ function mapnet(network: string): Network {
     }
 }
 
-(async function () {
-    const idToChainName = new Map<number, ChainName>()
-    for (const c of chains) {
-        idToChainName.set(toChainId(c), c)
-    }
+const idToChainName = new Map<number, ChainName>()
+for (const c of chains) {
+    idToChainName.set(toChainId(c), c)
+}
 
+function checkWhSdk() {
     for (const [network, chainContracts] of Object.entries(whcontracts)) {
         const net = mapnet(network);
         for (const [chain, contracts] of Object.entries(chainContracts)) {
@@ -64,4 +66,58 @@ function mapnet(network: string): Network {
                 console.log(`Relayer mismatch for ${id} ${net} ${chain}: ${contracts.wormholeRelayerAddress} vs ${newContracts.relayer}`)
         }
     }
+}
+
+
+(async function () {
+    const chains = cfg.getDocChains();
+    for (const chain of chains) {
+        const sdkName = idToChainName.get(chain.id)!
+
+        const { extraDetails } = chain
+        for (const network of ["Mainnet", "Testnet", "Devnet"]) {
+
+            // @ts-ignore
+            const sdkChain = newconf[network].chains[sdkName] as SdkChainConfig;
+
+            const sdkContracts = sdkChain.contracts as SdkContracts
+
+            // @ts-ignore
+            const docContracts = chain[network.toLowerCase()]
+
+            if (sdkContracts === undefined) continue
+            if (docContracts === undefined) continue
+
+            if (sdkContracts.coreBridge !== docContracts.core)
+                console.log(`Core bridge mismatch for ${sdkName} ${network}: ${sdkContracts.coreBridge} vs ${docContracts.core}`)
+            if (sdkContracts.tokenBridge !== docContracts.token_bridge)
+                console.log(`Token bridge mismatch for ${sdkName} ${network}: ${sdkContracts.tokenBridge} vs ${docContracts.token_bridge}`)
+            if (sdkContracts.nftBridge !== docContracts.nft_bridge)
+                console.log(`NFT bridge mismatch for ${sdkName} ${network}: ${sdkContracts.nftBridge} vs ${docContracts.nft_bridge}`)
+            if (sdkContracts.relayer !== docContracts.wormholeRelayerAddress)
+                console.log(`Relayer mismatch for ${sdkName} ${network}: ${sdkContracts.relayer} vs ${docContracts.wormholeRelayerAddress}`)
+            if (sdkContracts.cctp?.wormhole !== docContracts.cctp)
+                console.log(`CCTP mismatch for ${sdkName} ${network}: ${sdkContracts.cctp?.wormhole} vs ${docContracts.cctp}`)
+
+            if (!extraDetails) continue
+
+            // @ts-ignore
+            const nativeChainId = extraDetails[network.toLowerCase()]
+
+            if (network === "Devnet") continue;
+
+            if (!nativeChainId) {
+                console.log("Missing chain id for: " + sdkName + " " + network)
+                continue
+            }
+
+            if (nativeChainId.id !== "N/A" && nativeChainId.id !== sdkChain.nativeChainId)
+                console.log(`Native chain id mismatch for ${sdkName} ${network}: ${nativeChainId.id} vs ${sdkChain.nativeChainId}`)
+        }
+
+
+
+    }
+
 })()
+
