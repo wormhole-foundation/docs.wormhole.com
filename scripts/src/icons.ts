14395f8767f46bfa46ebc8f75b94646eddb41100
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-05-02 12:18:37 -0400

    update icons package and rerun script

--- scripts/src/icons.ts
+++ scripts/src/icons.ts
@@ -1,17 +1,14 @@
-import { coalesceChainName, ChainName } from "@certusone/wormhole-sdk";
-import { encoding, toChainId } from "@wormhole-foundation/sdk";
-import { chainIcons } from "@wormhole-foundation/sdk-icons";
+import { CHAINS } from "@certusone/wormhole-sdk";
+import { encoding, toChain } from "@wormhole-foundation/sdk";
+import { chainToIcon } from "@wormhole-foundation/sdk-icons";
 import * as fs from "fs";
 
 const ICONS_PATH = `${__dirname}/../../docs/.gitbook/assets/chain-icons`;
-
 const prefix = "data:image/svg+xml;base64,";
-for (const [chain, icon] of Object.entries(chainIcons)) {
+for (const [name, chainId] of Object.entries(CHAINS)) {
+  if (chainId === 0) continue;
   const svg = encoding.bytes.decode(
-    encoding.b64.decode(icon.slice(prefix.length))
+    encoding.b64.decode(chainToIcon(toChain(chainId)).slice(prefix.length))
   );
-  const name = coalesceChainName(toChainId(chain));
-
-  //console.log(`Writing ${name}.svg ${ICONS_PATH}/${name}.svg`);
   fs.writeFileSync(`${ICONS_PATH}/${name}.svg`, svg);
 }
